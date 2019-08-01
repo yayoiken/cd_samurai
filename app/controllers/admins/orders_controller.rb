ba14bc7598fc5d6a60b9f27e09d2573bfa9361e1
maedayuya <maeda.yuya.infratop@gmail.com>
2019-08-01 10:23:54 +0000

    ステータスの編集

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -8,16 +8,21 @@ class Admins::OrdersController < ApplicationController
     @order.order_products.each do |op|
       @genre = Genre.find(op.genre_id)
     end
+    @ship_status = OrderProduct.where(ship_status: :ship_status)
+
   end
 
   def update
   	@order = Order.find(params[:id])
   	@order.order_products.each do |op|
-  	  @ship_status = op.ship_status
+  	  op.update(order_product_params)
   	end
-  	@ship_status.save
   	redirect_to admins_orders_path
 
+  end
+
+  def order_product_params
+	params.require(:order_product).permit(:ship_status)
   end
 
 end
